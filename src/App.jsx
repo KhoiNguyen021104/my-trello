7e662f1c3881b0560462f0c8cb256cba34a6778a
KhoiNguyenDev <khoindt10a4@gmail.com>
2024-08-07 08:15:50 +0700

    Config theme mode system

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
+import './App.css'
+
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import Typography  from '@mui/material/Typography'
+import Typography from '@mui/material/Typography'
 import {
   Experimental_CssVarsProvider as CssVarsProvider,
   experimental_extendTheme as extendTheme,
-  useColorScheme,
+  useColorScheme
 } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Box from '@mui/material/Box'
+
+function SelectSmall() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light"><Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}><LightModeIcon fontSize='small'/> Light</Box></MenuItem>
+        <MenuItem value="dark"><Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}><DarkModeOutlinedIcon fontSize='small'/> Dark</Box></MenuItem>
+        <MenuItem value="system"><Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}><SettingsBrightnessIcon fontSize='small'/> System</Box></MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
 
 function ModeToggle() {
-  const { mode, setMode } = useColorScheme();
+  const { mode, setMode } = useColorScheme()
+  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  // const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)')
   return (
     <Button
       onClick={() => {
@@ -24,6 +61,8 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <SelectSmall />
+      <hr />
       <ModeToggle />
       <hr />
       <div>Khoi Nguyen Dev</div>
@@ -41,4 +80,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
