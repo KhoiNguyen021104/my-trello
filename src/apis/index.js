adc7d1f8b63e787e3f200fe3fa81a18ae19a7b0e
KhoiNguyenDev <khoindt10a4@gmail.com>
2024-08-17 08:14:52 +0700

    Axios call API Board

---
+++ src/apis/index.js
@@ -0,0 +1,9 @@
+// Apis
+import axios from 'axios'
+import { API_ROOT } from '~/utils/constants'
+
+export const fetchBoardDetailsAPI = async (boardId) => {
+  const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`)
+  // response => object => kết quả trong data của response
+  return response.data
+}
--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -4,18 +4,30 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { Box } from '@mui/material'
-import Sidebar from './Sidebar/Sidebar'
-import { mockData } from '~/apis/mock-data'
+// import Sidebar from './Sidebar/Sidebar'
+// import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
+  const [board, setBoard] = useState(null)
+  useEffect(() => {
+    // HardCode boardId = 66bf091ff8d0383207d508b7
+    const boardId = '66bf091ff8d0383207d508b7'
+    // Call Api
+    fetchBoardDetailsAPI(boardId)
+      .then((response) => {
+        setBoard(response)
+      })
+  }, [])
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh', maxHeight: '100vh', overflowY:'hidden !important' }}>
       <AppBar />
       <Box sx={{ display:'flex', height: (theme) => `calc(100vh - ${theme.app.APP_BAR_HEIGHT})`, overflow:'hidden' }}>
         {/* <Sidebar /> */}
         <Box sx={{ flex: 1, overflowX:'auto', overflowY:'hidden' }}>
-          <BoardBar board={ mockData?.board } />
-          <BoardContent board={ mockData?.board }/>
+          <BoardBar board={ board } />
+          <BoardContent board={ board }/>
         </Box>
       </Box>
     </Container>
